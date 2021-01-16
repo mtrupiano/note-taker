63da7effb970a8b30c8aae89c0cb83c830db5586
mtrupiano <markt6749@gmail.com>
2021-01-15 17:35:59 -0800

    drafting get requests

--- server.js
+++ server.js
@@ -7,3 +7,18 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", function (req, res) {
+    // display home HTML
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    // display home HTML
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+    // display home HTML
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
